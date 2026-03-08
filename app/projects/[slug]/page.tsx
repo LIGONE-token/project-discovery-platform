901dd89bd8cecdacc71b9250f7968d239c27e356
LIGONE Project <rene.herrchen@gmail.com>
2026-03-08 19:18:36 +0100

    Create page.tsx

---
+++ app/projects/[slug]/page.tsx
@@ -0,0 +1,20 @@
+import data from "../../../data/projects.json"
+
+export default function ProjectPage({ params }) {
+
+  const project = data.find(p => p.slug === params.slug)
+
+  if (!project) {
+    return <div>Project not found</div>
+  }
+
+  return (
+    <div>
+      <h1>{project.name}</h1>
+      <p>{project.description}</p>
+      <p>Category: {project.category}</p>
+
+      <a href={project.url}>Visit website</a>
+    </div>
+  )
+}
